b5fe33197fadba7b9c2a5bc3010322677f493c82
sanjoyk <sanjoyksardar12@gmail.com>
2020-07-31 05:04:04 +0530

    update version

--- src/index.js
+++ src/index.js
@@ -4,31 +4,28 @@ const path = require("path");
 var minify = require('html-minifier-terser').minify;
 
 function MinifyEJSContent(config) {
-  console.log("config", config);
   this.config = config;
-  this.init();
+  this.init(config);
 }
-MinifyEJSContent.prototype.init=function () {
+MinifyEJSContent.prototype.init=function (config) {
   this.config.minify = {
     collapseWhitespace: true,
-    ...(this.config.minify ||{})
+    ...(config.minify ||{})
   }
 }
 MinifyEJSContent.prototype.apply = function (compiler) {
-  compiler.plugin("done",  (compilation, callback)=> {
+  compiler.plugin("done",  function(stat, callback) {
     function workThrough(dir, minifyOption) {
-      console.log("minifyOption===", minifyOption);
       const files = fs.readdirSync(dir);
       files.forEach(filepath => {
         if (fs.statSync(dir + "/" + filepath).isDirectory()) {
-          workThrough(dir + "/" + filepath );
+          workThrough(dir + "/" + filepath ,minifyOption);
         } else if(filepath.includes(".ejs")) {
           fs.readFile(path.resolve(`${dir}/${filepath}`), "utf-8",  (err, data) =>{
             if(err){
               console.log(err , " to read file");
               return;
             }
-            console.log("minifyOption", minifyOption);
             const updatedContent = minify(data, {...minifyOption} );
             fs.writeFile(path.resolve(`${dir + "/"}${filepath}`), updatedContent, (err) => {
               if (err) console.log(err);
@@ -39,8 +36,8 @@ MinifyEJSContent.prototype.apply = function (compiler) {
         }
       });
     }
-    console.log("this.config", this.config);
-    workThrough(this.config.dirPath,this.config.minify);
+    const config = stat.compilation.options.plugins.filter(plugin=>plugin.constructor.name ==="MinifyEJSContent")[0].config;
+    workThrough(config.dirPath,config.minify);
   });
 }
 
