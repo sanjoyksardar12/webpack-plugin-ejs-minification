f208cda5c68de6a3071b24140fdae612513264a3
sanjoyk <sanjoyksardar12@gmail.com>
2020-07-31 05:03:27 +0530

    update readme
    fix option

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # webpack-plugin-ejs-minification
+Plugin that minify ejs using [html-minifier-terser](https://www.npmjs.com/package/html-minifier-terser)
+
+### installation
+```
+npm i --save-dev webpack-plugin-ejs-minification
+```
+```
+yarn add --dev webpack-plugin-ejs-minification
+```
+Add the plugin to your webpack config as follows:
+### webpack.config.js
+```
+const MinifyEJSContent = require('webpack-plugin-ejs-minification');
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new MinifyEJSContent({
+      dirPath: path.resolve(__dirname, `../dist/views`),
+      minify:{
+        minifyJS: true,
+        removeComments: true
+      }
+    }),
+  ]
+}
+```
+More options can be added from [https://github.com/DanielRuf/html-minifier-terser#options-quick-reference](https://github.com/DanielRuf/html-minifier-terser#options-quick-reference)
--- src/index.js
+++ src/index.js
@@ -4,30 +4,32 @@ const path = require("path");
 var minify = require('html-minifier-terser').minify;
 
 function MinifyEJSContent(config) {
+  console.log("config", config);
   this.config = config;
   this.init();
 }
 MinifyEJSContent.prototype.init=function () {
-  this.config.option = {
+  this.config.minify = {
     collapseWhitespace: true,
-    removeComments: true,
-    minifyJS: true,
-    ...(this.config.option ||{})
+    ...(this.config.minify ||{})
   }
 }
 MinifyEJSContent.prototype.apply = function (compiler) {
   compiler.plugin("done",  (compilation, callback)=> {
-    function workThrough(dir) {
+    function workThrough(dir, minifyOption) {
+      console.log("minifyOption===", minifyOption);
       const files = fs.readdirSync(dir);
       files.forEach(filepath => {
         if (fs.statSync(dir + "/" + filepath).isDirectory()) {
           workThrough(dir + "/" + filepath );
         } else if(filepath.includes(".ejs")) {
-          fs.readFile(path.resolve(`${dir}/${filepath}`), "utf-8", function (err, data) {
+          fs.readFile(path.resolve(`${dir}/${filepath}`), "utf-8",  (err, data) =>{
             if(err){
               console.log(err , " to read file");
+              return;
             }
-            const updatedContent = minify(data, { });
+            console.log("minifyOption", minifyOption);
+            const updatedContent = minify(data, {...minifyOption} );
             fs.writeFile(path.resolve(`${dir + "/"}${filepath}`), updatedContent, (err) => {
               if (err) console.log(err);
               const completeFilePath = `${dir + "/"}${filepath}`;
@@ -37,7 +39,8 @@ MinifyEJSContent.prototype.apply = function (compiler) {
         }
       });
     }
-    workThrough(this.config.dirPath);
+    console.log("this.config", this.config);
+    workThrough(this.config.dirPath,this.config.minify);
   });
 }
 
